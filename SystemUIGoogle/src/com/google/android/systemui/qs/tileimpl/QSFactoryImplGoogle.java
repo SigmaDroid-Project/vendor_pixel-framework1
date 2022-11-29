22cb3ca8186a93247ca2783807c412403fbb8e62
Matt Filetto <matt.filetto@gmail.com>
2022-11-29 07:50:26 -0800

    gs: tileImpl, try syncing with spark again

--- SystemUIGoogle/src/com/google/android/systemui/qs/tileimpl/QSFactoryImplGoogle.java
+++ SystemUIGoogle/src/com/google/android/systemui/qs/tileimpl/QSFactoryImplGoogle.java
@@ -22,10 +22,11 @@ import com.android.systemui.qs.QSHost;
 import com.android.systemui.qs.external.CustomTile;
 import com.android.systemui.qs.tileimpl.QSFactoryImpl;
 import com.android.systemui.qs.tileimpl.QSTileImpl;
+import com.android.systemui.qs.tiles.AODTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
 import com.android.systemui.qs.tiles.AlarmTile;
 import com.android.systemui.qs.tiles.AmbientDisplayTile;
-import com.android.systemui.qs.tiles.AODTile;
+import com.android.systemui.qs.tiles.AntiFlickerTile;
 import com.android.systemui.qs.tiles.BatterySaverTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
 import com.android.systemui.qs.tiles.CPUInfoTile;
@@ -42,28 +43,27 @@ import com.android.systemui.qs.tiles.DeviceControlsTile;
 import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.FPSInfoTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
-//import com.android.systemui.qs.tiles.FlashlightStrengthTile;
-//import com.android.systemui.qs.tiles.GloveModeTile;
 import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.InternetTile;
+import com.android.systemui.qs.tiles.LiveDisplayTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.MicrophoneToggleTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
-import com.android.systemui.qs.tiles.OnTheGoTile;
 import com.android.systemui.qs.tiles.OneHandedModeTile;
-import com.android.systemui.qs.tiles.PowerShareTile;
 import com.android.systemui.qs.tiles.QRCodeScannerTile;
+import com.android.systemui.qs.tiles.OnTheGoTile;
+import com.android.systemui.qs.tiles.PowerShareTile;
 import com.android.systemui.qs.tiles.QuickAccessWalletTile;
-import com.android.systemui.qs.tiles.ReadingModeTile;
-//import com.android.systemui.qs.tiles.RebootTile;
+import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.ReduceBrightColorsTile;
+import com.android.systemui.qs.tiles.ReadingModeTile;
 import com.android.systemui.qs.tiles.RefreshRateTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenRecordTile;
 import com.android.systemui.qs.tiles.SmartPixelsTile;
-//import com.android.systemui.qs.tiles.SoundSearchTile;
+import com.android.systemui.qs.tiles.SoundSearchTile;
 import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.UiModeNightTile;
@@ -74,11 +74,6 @@ import com.android.systemui.qs.tiles.WorkModeTile;
 import com.android.systemui.util.leak.GarbageMonitor;
 import com.google.android.systemui.qs.tiles.BatterySaverTileGoogle;
 import com.google.android.systemui.qs.tiles.ReverseChargingTile;
-
-//crdroid
-import com.android.systemui.qs.tiles.VpnTile;
-import com.android.systemui.qs.tiles.WeatherTile;
-import com.android.systemui.qs.tiles.ProfilesTile;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
@@ -124,78 +119,79 @@ public class QSFactoryImplGoogle extends QSFactoryImpl {
             Provider<QRCodeScannerTile> qrCodeScannerTileProvider,
             Provider<OneHandedModeTile> oneHandedModeTileProvider,
             Provider<ColorCorrectionTile> colorCorrectionTileProvider,
-            Provider<ReverseChargingTile> reverseChargingTileProvider,
+            Provider<OnTheGoTile> onTheGoTileProvider,
+            Provider<AODTile> aodTileProvider,
             Provider<PowerShareTile> powerShareTileProvider,
             Provider<CaffeineTile> caffeineTileProvider,
             Provider<AmbientDisplayTile> ambientDisplayTileProvider,
             Provider<UsbTetherTile> usbTetherTileProvider,
             Provider<SyncTile> syncTileProvider,
             Provider<SoundTile> soundTileProvider,
-            Provider<VolumeTile> volumeTileProvider,
-            Provider<ReadingModeTile> readingModeTileProvider,
-            Provider<DataSwitchTile> dataSwitchTileProvider,
-            Provider<AODTile> aodTileProvider,
-            Provider<RefreshRateTile> refreshRateTileProvider,
-            Provider<FPSInfoTile> fpsInfoTileProvider,
-            Provider<SmartPixelsTile> smartPixelsTileProvider,
-            Provider<OnTheGoTile> onTheGoTileProvider,
-            Provider<CPUInfoTile> cpuInfoTileProvider,
             Provider<HeadsUpTile> headsUpTileProvider,
+            Provider<RebootTile> rebootTileProvider,
+            Provider<VolumeTile> volumeTileProvider,
+            Provider<DataSwitchTile> dataSwitchTileProvider,
+            Provider<FPSInfoTile> fpsInfoTileProvider,
+            Provider<LiveDisplayTile> liveDisplayTileProvider,
+            Provider<ReadingModeTile> readingModeTileProvider,
+            Provider<AntiFlickerTile> antiFlickerTileProvider,
             Provider<CompassTile> compassTileProvider,
-            Provider<WeatherTile> weatherTileProvider,
-            Provider<VpnTile> vpnTileProvider,
-            Provider<ProfilesTile> profilesTileProvider) {
+            Provider<CPUInfoTile> cpuInfoTileProvider,
+            Provider<SoundSearchTile> soundSearchTileProvider,
+            Provider<SmartPixelsTile> smartPixelsTileProvider,
+            Provider<RefreshRateTile> refreshRateTileProvider) {
         super(qsHostLazy,
                 customTileBuilderProvider,
-                wifiTileProvider,
-                internetTileProvider,
-                bluetoothTileProvider,
-                cellularTileProvider,
-                dndTileProvider,
-                colorInversionTileProvider,
-                airplaneModeTileProvider,
-                workModeTileProvider,
-                rotationLockTileProvider,
-                flashlightTileProvider,
-                locationTileProvider,
-                castTileProvider,
-                hotspotTileProvider,
-                () -> batterySaverTileGoogleProvider.get(),
-                dataSaverTileProvider,
-                nightDisplayTileProvider,
-                nfcTileProvider,
-                memoryTileProvider,
-                uiModeNightTileProvider,
-                screenRecordTileProvider,
-                reduceBrightColorsTileProvider,
-                cameraToggleTileProvider,
-                microphoneToggleTileProvider,
-                deviceControlsTileProvider,
-                alarmTileProvider,
-                quickAccessWalletTileProvider,
-                qrCodeScannerTileProvider,
-                oneHandedModeTileProvider,
-                colorCorrectionTileProvider,
-		ambientDisplayTileProvider,
-		aodTileProvider,
-                caffeineTileProvider,
-                headsUpTileProvider,
-                powerShareTileProvider,
-                profilesTileProvider,
-                readingModeTileProvider,
-                syncTileProvider,
-                usbTetherTileProvider,
-                vpnTileProvider,
-                soundTileProvider,
-                onTheGoTileProvider,
-                cpuInfoTileProvider,
-                fpsInfoTileProvider,
-                compassTileProvider,
-                dataSwitchTileProvider,
-                volumeTileProvider,
-                smartPixelsTileProvider,
-                weatherTileProvider,
-                refreshRateTileProvider);
+        wifiTileProvider,
+         internetTileProvider,
+         bluetoothTileProvider,
+        cellularTileProvider,
+        dndTileProvider,
+        colorInversionTileProvider,
+        airplaneModeTileProvider,
+         workModeTileProvider,
+        rotationLockTileProvider,
+        flashlightTileProvider,
+        locationTileProvider,
+        castTileProvider,
+       hotspotTileProvider,
+  () -> batterySaverTileGoogleProvider.get(),
+        dataSaverTileProvider,
+        nightDisplayTileProvider,
+      nfcTileProvider,
+      memoryTileProvider,
+       uiModeNightTileProvider,
+        screenRecordTileProvider,
+        reduceBrightColorsTileProvider,
+        cameraToggleTileProvider,
+        microphoneToggleTileProvider,
+        deviceControlsTileProvider,
+       alarmTileProvider,
+        quickAccessWalletTileProvider,
+        qrCodeScannerTileProvider,
+        oneHandedModeTileProvider,
+        colorCorrectionTileProvider,
+        onTheGoTileProvider,
+       aodTileProvider,
+        powerShareTileProvider,
+       caffeineTileProvider,
+       ambientDisplayTileProvider,
+         usbTetherTileProvider,
+        syncTileProvider,
+        soundTileProvider,
+         headsUpTileProvider,
+         rebootTileProvider,
+         volumeTileProvider,
+        dataSwitchTileProvider,
+        fpsInfoTileProvider,
+        liveDisplayTileProvider,
+        readingModeTileProvider,
+        antiFlickerTileProvider,
+        compassTileProvider,
+        cpuInfoTileProvider,
+        soundSearchTileProvider,
+        smartPixelsTileProvider,
+         refreshRateTileProvider);
         mReverseChargingTileProvider = reverseChargingTileProvider;
         mBatterySaverTileGoogleProvider = batterySaverTileGoogleProvider;
     }
