6c9013d3928fdfa610ec3718f9b3c4b1f7714440
Matt Filetto <matt.filetto@gmail.com>
2023-05-23 20:04:34 -0700

    SystemUIGoogle: Sync with Spark
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- SystemUIGoogle/src/com/google/android/systemui/qs/tileimpl/QSFactoryImplGoogle.java
+++ SystemUIGoogle/src/com/google/android/systemui/qs/tileimpl/QSFactoryImplGoogle.java
@@ -46,7 +46,6 @@ import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.InternetTile;
-import com.android.systemui.qs.tiles.LocaleTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.MicrophoneToggleTile;
 import com.android.systemui.qs.tiles.NfcTile;
@@ -59,12 +58,9 @@ import com.android.systemui.qs.tiles.QRCodeScannerTile;
 import com.android.systemui.qs.tiles.QuickAccessWalletTile;
 import com.android.systemui.qs.tiles.ReadingModeTile;
 import com.android.systemui.qs.tiles.ReduceBrightColorsTile;
-import com.android.systemui.qs.tiles.RefreshRateTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenRecordTile;
-import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.SmartPixelsTile;
-import com.android.systemui.qs.tiles.SoundSearchTile;
 import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.UiModeNightTile;
@@ -140,11 +136,7 @@ public class QSFactoryImplGoogle extends QSFactoryImpl {
             Provider<CompassTile> compassTileProvider,
             Provider<DataSwitchTile> dataSwitchTileProvider,
             Provider<VolumeTile> volumeTileProvider,
-            Provider<SmartPixelsTile> smartPixelsTileProvider,
-            Provider<RefreshRateTile> refreshRateTileProvider,
-            Provider<ScreenshotTile> screenshotTileProvider,
-            Provider<SoundSearchTile> soundSearchTileProvider,
-            Provider<LocaleTile> localeTileProvider) {
+            Provider<SmartPixelsTile> smartPixelsTileProvider) {
         super(qsHostLazy,
                 customTileBuilderProvider,
                 wifiTileProvider,
@@ -194,11 +186,7 @@ public class QSFactoryImplGoogle extends QSFactoryImpl {
                 compassTileProvider,
                 dataSwitchTileProvider,
                 volumeTileProvider,
-                smartPixelsTileProvider,
-                refreshRateTileProvider,
-                screenshotTileProvider,
-                soundSearchTileProvider,
-                localeTileProvider);
+                smartPixelsTileProvider);
         mBatterySaverTileGoogleProvider = batterySaverTileGoogleProvider;
     }
 
