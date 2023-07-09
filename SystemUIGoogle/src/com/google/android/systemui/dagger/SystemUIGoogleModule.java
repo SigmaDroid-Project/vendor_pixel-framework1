9184fe008b53ea6c19512ad20f325d18c404a76b
Matt Filetto <matt.filetto@gmail.com>
2023-07-09 03:43:27 -0700

    SystemUIGoogle: EvolutionModule -> LineageModule
    * Update dagger module to use LineageOS classes
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- SystemUIGoogle/src/com/google/android/systemui/dagger/SystemUIGoogleModule.java
+++ SystemUIGoogle/src/com/google/android/systemui/dagger/SystemUIGoogleModule.java
@@ -39,7 +39,7 @@ import com.android.systemui.dock.DockManager;
 import com.android.systemui.dock.DockManagerImpl;
 import com.android.systemui.doze.DozeHost;
 import com.android.systemui.dump.DumpManager;
-import com.android.systemui.evolution.EvolutionModule;
+import com.android.systemui.lineage.LineageModule;
 import com.android.systemui.media.dagger.MediaModule;
 import com.android.systemui.navigationbar.gestural.GestureModule;
 import com.android.systemui.plugins.BcSmartspaceDataPlugin;
@@ -127,7 +127,7 @@ import dagger.Lazy;
         StatusBarEventsModule.class,
         AospPolicyModule.class,
         RotationLockModule.class,
-        EvolutionModule.class
+        LineageModule.class
 })
 public abstract class SystemUIGoogleModule {
 
